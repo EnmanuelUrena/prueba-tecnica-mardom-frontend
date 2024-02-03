27d5499e0af92bb65ddea4417890eef8516a89e3
EnmanuelUrena <enmanuelurenav@gmail.com>
2024-02-03 14:28:57 -0400

    Front angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './Models/Employee.model';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
+  providers: [EmployeeService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'prueba-tecnica-mardom-frontend';
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.employeeService.getEmployees().subscribe((data: Employee[]) => {
+      this.employees = data;
+    },
+      () => {
+        alert('Error al cargar los empleados')
+      }
+    );
+  }
 }
